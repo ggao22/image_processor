cfee6dc0c3bfc5f93177613efa462be47169498a
ggao22 <g.gao@wustl.edu>
2022-07-06 19:05:30 -0500

    slight changes

--- image_processor/image_processor_node.py
+++ image_processor/image_processor_node.py
@@ -3,19 +3,21 @@ import rclpy
 from rclpy.node import Node
 from sensor_msg.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-
+import cv2
 
 class ImageProcessorNode(Node):
 
     def __init__(self):
         super().__init__('image_processor')
-        self.subscriber_ = self.create_subscription(Image, '/raw', self.image_callback, 10)
+        self.subscriber_ = self.create_subscription(Image, '/raw_frame', self.image_callback, 10)
         self.subscriber_
         self.bridge = CvBridge()
 
     def image_callback(self,data):
         try:
-           cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8") # TODO: adding image processing
+           cv_frame = self.bridge.imgmsg_to_cv2(data, "bgr8") # TODO: adding image processing
+           cv2.imshow("camera", cv_frame)
+           cv2.waitKey(1)
         except CvBridgeError as e:
             print(e) # TODO: Error handing
 
